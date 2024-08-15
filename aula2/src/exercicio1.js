7541fb20defb99e0a810d4bd338bd21f832b57de
joaoestreano <joaovictorestreano@gmail.com>
2024-08-14 21:54:29 -0300

    Add files via upload

--- aula2/src/exercicio1.js
+++ aula2/src/exercicio1.js
@@ -1,11 +1,31 @@
-numero1=prompt("Entre com um Numero");
-numero1=parseInt(numero1);
-numero2=prompt("Entre com outro Numero");
-numero2=parseInt(numero2);
-numero=numero1%numero2
+numero1 = prompt("Entre com um Numero");
+numero1 = parseInt(numero1);
+numero2 = prompt("Entre com outro Numero");
+numero2 = parseInt(numero2);
+numero = numero1 % numero2;
 
+numero = prompt("entre com um numero");
+numero = parseInt(numero);
+if (numero % 2 == 0) {
+  console.log(numero, "Par");
+} else {
+  console.log(numero, "impar");
+}
 
-numero=prompt("entre com um numero");
-numero=parseInt(numero);
-if(numero%2==0) {console.log("Par")}
-else(numero%2==1) {console.log("Impar")};
+numero = prompt("entre com um numero");
+numero = parseInt(numero);
+if (numero % 2 == 0) {
+  console.log(prompt("par"));
+} else {
+  console.log(prompt(numero, "impar"));
+}
+
+numero1 = prompt("Entre com um Numero");
+numero1 = parseInt(numero1);
+numero2 = prompt("Entre com outro Numero");
+numero2 = parseInt(numero2);
+if (numero1 % numero == 0) {
+  prompt(numero, "multiplo");
+} else {
+  prompt(numero, "não é multiplo");
+};
