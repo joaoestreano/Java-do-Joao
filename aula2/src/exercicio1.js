2f07052ba1daeb199a80774594f4f257701e882b
joaoestreano <joaovictorestreano@gmail.com>
2024-08-14 21:49:04 -0300

    Aula 2 de algoritmo

---
+++ aula2/src/exercicio1.js
@@ -0,0 +1,11 @@
+numero1=prompt("Entre com um Numero");
+numero1=parseInt(numero1);
+numero2=prompt("Entre com outro Numero");
+numero2=parseInt(numero2);
+numero=numero1%numero2
+
+
+numero=prompt("entre com um numero");
+numero=parseInt(numero);
+if(numero%2==0) {console.log("Par")}
+else(numero%2==1) {console.log("Impar")};
