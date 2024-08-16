0dd87acfb303b94dc9d0285ebfcc8e81a9df7ee3
joaoestreano <joaovictorestreano@gmail.com>
2024-08-16 19:40:54 -0300

    Add files via upload

--- aula2/src/exercicio1.js
+++ aula2/src/exercicio1.js
@@ -6,98 +6,98 @@ numero2 = parseInt(numero2); //converte o texto em numero
 numero = numero1 % numero2; //% mostra o resto da divição dos numeros
 
 //exercicio 2
-numero = prompt("entre com um numero");
+numero = prompt("entre com um numero");//o numero inserido sera "texto"
 numero = parseInt(numero); //converte o texto em numero
-if (numero % 2 == 0) {
-  console.log(numero, "Par");
-} else {
-  console.log(numero, "impar");
+if (numero % 2 == 0) {//se o "numero" dividido por 2 tiver resto = a 0
+  console.log(numero, "Par");//ele sera par
+} else {//se não...
+  console.log(numero, "impar");//sera impar
 }
 
 //exercicio 3
-numero = prompt("entre com um numero");
+numero = prompt("entre com um numero");//o numero inserido sera "texto"
 numero = parseInt(numero); //converte o texto em numero
-if (numero % 2 == 0) {
-  console.log(prompt("par"));
-} else {
-  console.log(prompt(numero, "impar"));
+if (numero % 2 == 0) {//se o "numero" dividido por 2 tiver resto = a 0
+  console.log(prompt("par"));//ele sera par
+} else {//se não...
+  console.log(prompt(numero, "impar"));//sera impar
 }
 
 //exercicio 4
-numero1 = prompt("Entre com um Numero");
+numero1 = prompt("Entre com um Numero");//o numero inserido sera "texto"
 numero1 = parseInt(numero1); //converte o texto em numero
-numero2 = prompt("Entre com outro Numero");
+numero2 = prompt("Entre com outro Numero");//o numero inserido sera "texto"
 numero2 = parseInt(numero2); //converte o texto em numero
-if (numero1 % numero == 0) {
-  prompt(numero, "multiplo");
-} else {
-  prompt(numero, "não é multiplo");
+if (numero1 % numero == 0) {//se "numero1" dividido por "numero" = a0
+  prompt(numero, "multiplo");//então sera "multiplo"
+} else {//se não...
+  prompt(numero, "não é multiplo");//então "não sera multiplo"
 }
 
 
 //exercicio 4
-numero1 = prompt("Entre com um Numero");
+numero1 = prompt("Entre com um Numero");//o numero inserido sera "texto"
 numero1 = parseInt(numero1); //converte o texto em numero
-numero2 = prompt("Entre com outro Numero");
+numero2 = prompt("Entre com outro Numero");//o numero inserido sera "texto"
 numero2 = parseInt(numero2); //converte o texto em numero
-if (numero1 > numero2) {
-  prompt(numero1);
-} else {
-  prompt(numero2);
+if (numero1 > numero2) {//se "numero1"for maior que "numero2"
+  prompt(numero1);//o maior sera "numero1"
+} else {//se não...
+  prompt(numero2);//o maior sera "numero2"
 }
 
 //exercicio 5
-numero = prompt("Entre com sua idade");
+numero = prompt("Entre com sua idade");//o numero inserido sera "texto"
 numero = parseInt(numero); //converte o texto em numero
-if (numero < 18) {
-  console.log("menor de idade");
-} else {
-  console.log("maior de idade");
+if (numero < 18) {//se "numero" for menor que 18
+  console.log("menor de idade");//sera menor de idade
+} else {//se não...
+  console.log("maior de idade");//sera maior de idade
 }
 
 //exercicio 6
-a = prompt("Entre com um Numero");
+a = prompt("Entre com um Numero");//o numero inserido sera "texto"
 a = parseInt(a); //converte o texto em numero
-b = prompt("Entre com outro Numero");
+b = prompt("Entre com outro Numero");//o numero inserido sera "texto"
 b = parseInt(b); //converte o texto em numero
-c = prompt("entre com mais um numero");
+c = prompt("entre com mais um numero");//o numero inserido sera "texto"
 c = parseInt(c); //converte o texto em numero
-if (a <= b && b <= c) {
-  console.log("maior", c);
-} else {
-  if (a <= b && c <= b) {
-    console.log("maior", b);
-  } else {
-    console.log("maior", a);
+if (a <= b && b <= c) {//se a for menor ou igual a b, e b menor ou igual a c
+  console.log("maior", c);//então c sera o maior
+} else {//se não...
+  if (a <= b && c <= b) {//se a for menor ou igual a b, e c menor ou igual a b
+    console.log("maior", b);//então b sera o maior
+  } else {//se não...
+    console.log("maior", a);//se a for menor ou igual a b, e c menor ou igual a b
   }
 }
 console.log("Fim do Programa");
 
 //exercicio 7
-temperatura = prompt("entre com sua temperatura corporal:");
-temperatura = parseInt(temperatura);
-if (temperatura <= 35) {
-  console.log("hipotermia");
-} else {
-  if (temperatura <= 37.7) {
-    console.log("normal");
-  } else {
-    if (temperatura <= 39.5) {
-      console.log("febre");
-    } else {
-      if (temperatura <= 41) {
-        console.log("febre alta");
-      } else {
-        console.log("hipotermia");
+temperatura = prompt("entre com sua temperatura corporal:");//o numero inserido sera "texto"
+temperatura = parseInt(temperatura);//converte o texto em numero
+if (temperatura <= 35) {//se a temperatura for menor ou igual a 35
+  console.log("hipotermia");//sera "hipotermia"
+} else {//se não...
+  if (temperatura <= 37.7) {//se a temperatura for menor ou igual a 37.7
+    console.log("normal");//sera "normal"
+  } else {//se não...
+    if (temperatura <= 39.5) {//se a temperatura for menor ou igual a 39.5
+      console.log("febre");//sera "febre"
+    } else {//se não
+      if (temperatura <= 41) {//se a temperatura for menor ou igual a 41
+        console.log("febre alta");//sera "febre alta"
+      } else {//se não...
+        console.log("hipotermia");//sera "hipotermia"
       }
     }
   }
 }
 
 //exercicio 8
-peso=prompt("entre com o peso (kg):")
+peso=prompt("entre com o peso (kg):")//o numero inserido sera "texto"
 peso=parseFloat(peso)//parsefloat é para converter p/ numeros quebrados ex:65.4
-altura=prompt("entre com a altura (M):")
+altura=prompt("entre com a altura (M):")//o numero inserido sera "texto"
 altura=parseFloat(altura)
 imc=peso/(altura*altura)
 if(imc<18.5){
