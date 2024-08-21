582ff9eb667fc61005077d91fac7abc8154cc33b
joaoestreano <joaovictorestreano@gmail.com>
2024-08-21 19:28:28 -0300

    Add files via upload

--- aula2/src/exercicio1.js
+++ aula2/src/exercicio1.js
@@ -75,7 +75,7 @@ console.log("Fim do Programa");
 
 //exercicio 7
 temperatura = prompt("entre com sua temperatura corporal:");//o numero inserido sera "texto"
-temperatura = parseInt(temperatura);//converte o texto em numero
+temperatura = parseFloat(temperatura);///parsefloat é para converter p/ numeros quebrados ex:65.4
 if (temperatura <= 35) {//se a temperatura for menor ou igual a 35
   console.log("hipotermia");//sera "hipotermia"
 } else {//se não...
@@ -98,29 +98,29 @@ if (temperatura <= 35) {//se a temperatura for menor ou igual a 35
 peso=prompt("entre com o peso (kg):")//o numero inserido sera "texto"
 peso=parseFloat(peso)//parsefloat é para converter p/ numeros quebrados ex:65.4
 altura=prompt("entre com a altura (M):")//o numero inserido sera "texto"
-altura=parseFloat(altura)
-imc=peso/(altura*altura)
-if(imc<18.5){
-console.log("abaixo do peso")
+altura=parseFloat(altura)//parsefloat é para converter p/ numeros quebrados ex:65.4
+imc=peso/(altura*altura)//calculo do imc
+if(imc<18.5){//se imc for...
+console.log("abaixo do peso")//sera "abaixo do peso"
 }
-else{
-  if(imc<24,9){
-    console.log("peso ideal")
+else{//se não...
+  if(imc<24.9){//se imc for menor que 24.9
+    console.log("peso ideal")//sera "peso ideal"
   }
-  else{
-    if(imc<29.9){
-      console.log("levemente acima do peso")
+  else{//se não...
+    if(imc<29.9){//se imc for menor que 29.9
+      console.log("levemente acima do peso")//sera "levemente acima do peso"
     }
-    else{
-      if(imc<34.9){
-        console.log("obesidade grau I")
+    else{//se não...
+      if(imc<34.9){//se imc for menor que 34.9
+        console.log("obesidade grau I")//sera "obesidade grau I"
       }
-      else{
-        if(imc<39.9){
-          console.log("obesidade grau II")
+      else{//se não...
+        if(imc<39.9){//se imc for menor que 39.9
+          console.log("obesidade grau II")//sera "obesidade grau II"
         }
-        else{
-          console.log("obesidade grau III")
+        else{//se não...
+          console.log("obesidade grau III")//sera "obesidade grau III"
         }
       }
     }
